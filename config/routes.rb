5e23b3c66b6263e2cb177c5626b2be580a28e2d0
ramalakkshmi <ramalakkshmi26@gmail.com>
2022-08-03 15:25:14 +0530

    messsage

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   resources :words
  
   
-  get 'home/index'
+  #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
   root 'home#index'
   
+  get '/words', to: 'word#index'
+  post '/word', to: 'word#new'
+  get '/error', to: 'word#error'
 end
